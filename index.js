6f4c809bb01b0a1ef06e69649db881e68647fc87
Blackman37 <jan.cernousek@post.cz>
2021-03-28 22:43:10 +0200

    add get route tenants

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const app = express()
 app.use(express.json())
 
 
-app.post('/tenants', async(req, res) => {
+app.post('/tenants', async (req, res) => {
     const { username, password } = req.body
 
     try{
@@ -22,6 +22,21 @@ app.post('/tenants', async(req, res) => {
 })
 
 
+app.get('/tenants/:tenantId', async (req, res) => {
+    const tenantId = req.params.tenantId
+
+    try{
+        const user = await User.findOne({ 
+            where: { tenantId }
+        })
+
+        return res.json(user)
+    } catch(err) {
+        console.log(err)
+
+        return res.status(500).json(err)
+    }
+})
 
 app.listen({ port: 3001 }, async () => {
     console.log('Server is listening on http://localhost:3001')
