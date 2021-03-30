846538c36677beaf89ceed4f17c9d61e0ef92c54
Blackman37 <jan.cernousek@post.cz>
2021-03-30 21:17:51 +0200

    add pagination to get route for all articles

--- index.js
+++ index.js
@@ -12,11 +12,11 @@ app.use(express.json())
 app.post('/tenants', async (req, res) => {
     const { username, password } = req.body
 
-    try{
+    try {
         const user = await User.create({ username, password })
 
         return res.status(201).json(user)
-    } catch(err) {
+    } catch (err) {
         console.log(err)
 
         return res.status(500).json(err)
@@ -27,11 +27,11 @@ app.post('/tenants', async (req, res) => {
 app.get('/tenants/:tenantId', async (req, res) => {
     const { tenantId } = req.params
 
-    try{
+    try {
         const user = await User.findOne({ where: { tenantId } })
 
         return res.json(user)
-    } catch(err) {
+    } catch (err) {
         console.log(err)
 
         return res.status(500).json({ error: 'Something went wrong' })
@@ -48,21 +48,21 @@ app.post('/articles', async (req, res) => {
         const article = await Article.create({ title, perex })
 
         return res.status(201).json(article)
-    } catch(err) {
+    } catch (err) {
         console.log(err)
 
-        return res.status(400).json( err )
+        return res.status(400).json(err)
     }
 })
 
 app.get('/articles/:articleId', async (req, res) => {
     const { articleId } = req.params
 
-    try{
+    try {
         const article = await Article.findOne({ where: { articleId } })
 
         return res.json(article)
-    } catch(err) {
+    } catch (err) {
         console.log(err)
 
         return res.status(500).json({ error: 'Something went wrong' })
@@ -71,7 +71,19 @@ app.get('/articles/:articleId', async (req, res) => {
 
 app.get('/articles', async (req, res) => {
     try {
-        const articles = await Article.findAll()
+        const { offset = 0, limit = 0 } = req.query
+
+        let articles
+
+        if (limit > 0) {
+            articles = await Article.findAndCountAll({
+                limit: limit,
+                offset: offset,
+            })
+
+        } else {
+            articles = await Article.findAndCountAll()
+        }
 
         return res.json(articles)
     } catch (err) {
@@ -84,13 +96,13 @@ app.get('/articles', async (req, res) => {
 app.delete('/articles/:articleId', async (req, res) => {
     const { articleId } = req.params
 
-    try{
+    try {
         const article = await Article.findOne({ where: { articleId } })
 
         await article.destroy()
 
         return res.json({ message: 'Article deleted!' })
-    } catch(err) {
+    } catch (err) {
         console.log(err)
 
         return res.status(500).json({ error: 'Something went wrong' })
@@ -101,21 +113,21 @@ app.patch('/articles/:articleId', async (req, res) => {
     const { articleId } = req.params
     const { title, perex } = req.body
 
-    try{
+    try {
         const article = await Article.findOne({ where: { articleId } })
 
-        if(perex) {
+        if (perex) {
             article.perex = perex
         }
 
-        if(title) {
+        if (title) {
             article.title = title
         }
 
         await article.save()
 
         return res.json(article)
-    } catch(err) {
+    } catch (err) {
         console.log(err)
 
         return res.status(500).json({ error: 'Something went wrong' })
